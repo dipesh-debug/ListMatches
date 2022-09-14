7b20d83b869fe13e4ae88fb91a1dc68b5faa5de7
dipesh-debug <dipeshhud@gmail.com>
2022-09-15 00:03:46 +0545

    Listing Matches without css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// export default class FetchUser extends React.Component{
+//   state ={
+//     loading :true,
+//     person : null 
+//   };
+//   async componentDidMount(){
+   
+//     const url = "http://127.0.0.1:8000/api/streams/matches";
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     this.setState({person: data.results[0], loading : false});
+    
+
+//   }
+ 
+//   render(){
+//     if(this.state.loading){
+//       return <div> loading..</div>
+//     }
+//     if (!this.state.person){
+//       return <div> didn't get a person </div>
+
+//     }
+//     return( 
+     
+//       <div>
+//         <div>{this.state.person.id}</div>
+//         <div>{this.state.person.description}</div>
+//         <div>{this.state.person.embed_code}</div>
+//         {/* <img src = {this.state.person.picture.large}/> */}
+//       </div>
+      
+    
+   
+//     );
+//   }
+
+// }
+// import React from 'react';
+// import './App.css'  ;
+// import { useState} from 'react' ;
+// import axios  from 'axios' ;
+
+// function App ( ) {
+//   const [ quote , setQuote ] = useState ( ' ' )
+//   const getQuote = () => {
+//     axios.get ( 'http://127.0.0.1:8000/api/streams/matches?format=json' )
+//     .then ( res => {
+//       console.log ( res.id )
+//     setQuote ( res.description)
+//     } ).catch ( err => {
+//       console.log (err)
+//    } )
+//  }
+//   return (
+//     <div className='App'>
+//       <button onClick={getQuote}>Get quote</button>
+//       {quote && <p>{quote}</p> }
+//     </div>
+//   );
+// }
+// export default App;
+// import React, { useState } from 'react';
+// import Axios from 'axios' ; 
+// import './App.css';
+
+// function App ( ) {
+//   const[Match,setMatches] = useState ("");
+//   const getMatches = ( )  => {
+//     Axios.get(" http://127.0.0.1:8000/api/streams/matches?format=json ").then (
+
+//       ( response ) => {
+//         // setMatches(response.data.id + "..." + response.data.description);
+//         console.log(response);
+//         setMatches(response.data.id + "..." + response.data.description);
+//       }
+//     );
+//     // fetch("http://127.0.0.1:8000/api/streams/matches?format=json").then(
+//     //   (response) =>
+  
+//     //    response.json()).then((data) => {
+//     //     setMatches(data.id +"..."+ data.description);
+        
+      
+    
+//  };
+//   return (
+//     <div>
+
+//      Welcome <button onClick ={getMatches}> Get Matches </button>
+//      {Match}
+//     </div>
+//   );
+// }
+// export default App ;
+import React from "react";
+import './App.css';
+class App extends React.Component {
+
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			datas: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+"http://127.0.0.1:8000/api/streams/matches?format=json")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					datas: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, datas } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> List of Matches with their Embed code </h1> {
+				datas.map((data) => (
+				<ul >
+          <li>Match_id:{data.id},</li>
+					<li>Description : { data.description },</li>
+          <li>embded_code: <a href = { data.embed_code}>{data.embed_code}</a></li>
+					{/* <li>status: { data.status }</li> */}
+					</ul>
+				))
+			}
+		</div>
+	);
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// var cors = require('cors')
+
+// App.use(cors())
+// const cors = require('cors');
+// App.use(cors({ origin: true }))
+// const cors = require('cors');
+// App.use(cors({ origin: true }))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
